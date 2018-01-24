348c6c3b689acdb18429d88eb001b8dfefeb4293
Michal Rostecki <mrostecki@suse.com>
2018-01-24 12:48:25 +0100

    ubuntu: Separate the scripts specific for Ubuntu
    After this change, the scripts specific for distributions (which
    i.e. install packages) should stay in the separate subdirectories
    inside provision/.
    
    Signed-off-by: Michal Rostecki <mrostecki@suse.com>

--- provision/ubuntu/install.sh
+++ provision/ubuntu/install.sh
@@ -2,7 +2,6 @@
 
 GOLANG_VERSION="1.8.3"
 ETCD_VERSION="v3.1.0"
-CERTS_DIR=/certs/
 
 CLANG_DIR="clang+llvm-3.8.1-x86_64-linux-gnu-ubuntu-16.04"
 CLANG_FILE="${CLANG_DIR}.tar.xz"
@@ -89,13 +88,7 @@ sudo curl -Sslk -o go.tar.gz \
 sudo tar -C /usr/local -xzf go.tar.gz && \
 sudo rm go.tar.gz && \
 sudo ln -s /usr/local/go/bin/* /usr/local/bin/ && \
-go version &&\
-sudo mkdir /go/ &&\
-export GOPATH=/go/ &&\
-go get -u github.com/jteeuwen/go-bindata/... && \
-go get -u github.com/google/gops && \
-go get -u github.com/golang/protobuf/protoc-gen-go && \
-sudo ln -sf /go/bin/* /usr/local/bin/
+go version
 
 #Install docker compose
 sudo sh -c "curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
@@ -122,67 +115,3 @@ EOF
 
 sudo systemctl enable etcd
 sudo systemctl start etcd
-
-#Docker registry - certs
-
-sudo mkdir -p $CERTS_DIR
-sudo chmod 777 $CERTS_DIR
-cd $HOME
-rm -rfv certs
-mkdir certs
-
-cat <<EOF > server.conf
-prompt = no
-distinguished_name = req_distinguished_name
-req_extensions = v3_req
-
-[ req_distinguished_name ]
-C                      = UK
-ST                     = UK
-L                      = London
-O                      = cilium
-OU                     = experimental
-CN                     = cilium.io
-emailAddress           = ian@cilium.io
-
-[ v3_req ]
-# Extensions to add to a certificate request
-basicConstraints = CA:FALSE
-keyUsage = nonRepudiation, digitalSignature, keyEncipherment
-subjectAltName = @alt_names
-
-[ alt_names ]
-DNS.1 = cilium.io
-DNS.2 = *.cilium.io
-DNS.3 = k8s1
-IP.1 = 192.168.36.11
-IP.2 = 10.0.2.15
-EOF
-
-openssl genrsa -out certs/ca.key 4096
-openssl req -new -x509 -days 3650 -key certs/ca.key -out certs/ca.crt \
-    -subj "/C=uk/ST=uk/L=London/O=cilium/CN=cilium.io"
-
-openssl genrsa -out certs/cilium.key 4096
-openssl req -new -nodes \
-    -key certs/cilium.key \
-    -out certs/cilium.request -config server.conf
-
-openssl x509 -req -days 366 \
-    -in certs/cilium.request \
-    -CA certs/ca.crt \
-    -CAkey certs/ca.key \
-    -set_serial 01 \
-    -out certs/cilium.cert \
-    -extensions v3_req -extfile server.conf
-
-
-cp -rfv certs/* /certs/
-cp certs/ca.crt /usr/local/share/ca-certificates/
-sudo update-ca-certificates
-
-docker run -d -p 5000:5000 --name registry -v ${CERTS_DIR}:/certs \
-        -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/cilium.cert \
-        -e REGISTRY_HTTP_TLS_KEY=/certs/cilium.key \
-        --restart=always \
-        registry:2
