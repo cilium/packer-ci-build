c3bce14e90f5bf82f5789be0c448af09d978eb43
André Martins <andre@cilium.io>
2018-02-02 17:29:53 +0100

    provision: Change GOPATH to $HOME/go
    Signed-off-by: André Martins <andre@cilium.io>

--- provision/golang.sh
+++ provision/golang.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-sudo mkdir /go/ &&\
-export GOPATH=/go/ &&\
+sudo mkdir /home/vagrant/go &&\
+export GOPATH=/home/vagrant/go &&\
 go get -u github.com/jteeuwen/go-bindata/... && \
 go get -u github.com/google/gops && \
-go get -u github.com/golang/protobuf/protoc-gen-go && \
-sudo ln -sf /go/bin/* /usr/local/bin/
+go get -u github.com/golang/protobuf/protoc-gen-go
+
+# symlink all go bins to /usr/bin so envoy can use them without
+# changing envoy's PATH to include $GOPATH/bin
+sudo ln -s /home/vagrant/go/bin/* /usr/local/bin
