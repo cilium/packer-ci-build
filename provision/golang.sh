856c570e80a33c5fb1d29f5c91d08b2eaf78084b
André Martins <andre@cilium.io>
2018-02-07 16:36:34 +0100

    Replace jteeuwen/go-bindata with cilium/go-bindata
    Signed-off-by: André Martins <andre@cilium.io>

--- provision/golang.sh
+++ provision/golang.sh
@@ -3,6 +3,6 @@
 source "${ENV_FILEPATH}"
 
 sudo -E mkdir "${GOPATH}" && \
-go get -u github.com/jteeuwen/go-bindata/... && \
+go get -u github.com/cilium/go-bindata/... && \
 go get -u github.com/google/gops && \
 go get -u github.com/golang/protobuf/protoc-gen-go
