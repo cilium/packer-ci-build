9aa6e7afa80c0b7370daec7b514905f1357f55f2
Michal Rostecki <mrostecki@suse.com>
2018-02-12 15:41:48 +0100

    opensuse: Bump golang version to 1.9.x
    Signed-off-by: Michal Rostecki <mrostecki@suse.com>

--- provision/opensuse/install.sh
+++ provision/opensuse/install.sh
@@ -2,7 +2,9 @@
 
 set -eux
 
+sudo zypper -n ar -r https://download.opensuse.org/repositories/devel:/languages:/go/openSUSE_Factory/devel:languages:go.repo
 sudo zypper -n ar -r https://download.opensuse.org/repositories/home:/mrostecki/openSUSE_Tumbleweed/home:mrostecki.repo
 sudo zypper -n --gpg-auto-import-key in --no-recommends \
+        go1.9 \
         python3-sphinxcontrib-openapi \
     && sudo zypper clean
