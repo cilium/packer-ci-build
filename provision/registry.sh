44c8e5a8eec48eb17f94208c4f3ef56fe10241d1
André Martins <andre@cilium.io>
2018-02-16 21:48:51 +0100

    fix Not a directory error in opensuse build
    Signed-off-by: André Martins <andre@cilium.io>

--- provision/registry.sh
+++ provision/registry.sh
@@ -59,6 +59,7 @@ openssl x509 -req -days 366 \
     -out certs/cilium.cert \
     -extensions v3_req -extfile server.conf
 
+mkdir -p /usr/local/share/ca-certificates
 
 cp -rfv certs/* /certs/
 cp certs/ca.crt /usr/local/share/ca-certificates/
