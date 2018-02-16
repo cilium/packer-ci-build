1f265ba817cee407b0f91c648b64ce4450e54595
André Martins <andre@cilium.io>
2018-02-16 23:33:48 +0100

    synlink golang binaries to /usr/bin
    Signed-off-by: André Martins <andre@cilium.io>

--- provision/golang.sh
+++ provision/golang.sh
@@ -8,3 +8,5 @@ sudo -E mkdir "${GOPATH}" && \
 go get -u github.com/cilium/go-bindata/... && \
 go get -u github.com/google/gops && \
 go get -u github.com/golang/protobuf/protoc-gen-go
+
+sudo -E ln -s "${GOPATH}/bin/"* /usr/bin
