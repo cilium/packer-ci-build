a1cc3835d2b4ce8ffc556fedea46cd5319a1d508
Eloy Coto <eloy.coto@gmail.com>
2018-03-06 15:15:06 +0100

    Added protoc-gen-validate as dependency
    Related with https://github.com/cilium/cilium/commit/1218c6424015c05a13c22fa2feda3c5533c81801
    
    Signed-off-by: Eloy Coto <eloy.coto@gmail.com>

--- provision/golang.sh
+++ provision/golang.sh
@@ -7,6 +7,12 @@ set -e
 sudo -E mkdir "${GOPATH}" && \
 go get -u github.com/cilium/go-bindata/... && \
 go get -u github.com/google/gops && \
-go get -u github.com/golang/protobuf/protoc-gen-go
+go get -u github.com/golang/protobuf/protoc-gen-go && \
+go get -u github.com/lyft/protoc-gen-validate
+
+#Protoc-gen-validate installation
+cd $GOPATH/src/github.com/lyft/protoc-gen-validate
+sudo git checkout 930a67cf7ba41b9d9436ad7a1be70a5d5ff6e1fc
+make build
 
 sudo -E ln -s "${GOPATH}/bin/"* /usr/bin
