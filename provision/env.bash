4aeb9264dd914a1ec8f4ad4957709dc0dc09050b
Romain Lenglet <romain@covalent.io>
2018-03-07 14:28:58 -0800

    Upgrade Bazel to version 0.11.0
    Signed-off-by: Romain Lenglet <romain@covalent.io>

--- provision/env.bash
+++ provision/env.bash
@@ -3,7 +3,7 @@
 export GOLANG_VERSION="1.9.3"
 export ETCD_VERSION="v3.1.0"
 export DOCKER_COMPOSE_VERSION="1.16.1"
-export BAZEL_VERSION="0.10.1"
+export BAZEL_VERSION="0.11.0"
 
 export CLANG_ROOT=/usr/local/clang
 export HOME_DIR=/home/vagrant
