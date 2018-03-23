a165b212234ede605bc65ed11d20862a49b5a578
Eloy Coto <eloy.coto@gmail.com>
2018-03-23 10:56:58 +0100

    Add cilium/starwars image
    Jenkins build are slow because need to donwload that image.
    
    https://jenkins.cilium.io/job/Ginkgo-CI-Tests-Pipeline/1812/testReport/k8s-1/9/K8sValidatedDemosTest_Tests_Star_Wars_Demo/
    
    Signed-off-by: Eloy Coto <eloy.coto@gmail.com>

--- provision/pull-images.sh
+++ provision/pull-images.sh
@@ -2,13 +2,13 @@
 
 source "${ENV_FILEPATH}"
 
-set -e 
+set -e
 
 for img in tgraf/netperf httpd cilium/demo-httpd \
   cilium/demo-client borkmann/misc \
   registry busybox:latest quay.io/coreos/etcd:v3.1.0 \
   digitalwonderland/zookeeper wurstmeister/kafka \
-  cilium/kafkaclient2 ; do
+  cilium/kafkaclient2 cilium/starwars; do
   sudo docker pull $img &
 done
 
