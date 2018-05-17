b0774aeb9f700467bef48ffdbb03b171eb3f18e3
Jarno Rajahalme <jarno@covalent.io>
2018-05-17 13:37:08 -0700

    envoy: Use Bazel version of the cloned Cilium branch.
    The cloned Cilium branch contains 'envoy/BAZEL_VERSION' file that
    specified which version of Bazel to install. Use it so that we do not
    need to keep updating the packer-ci-build repo every time Bazel
    version is bumped.
    
    Signed-off-by: Jarno Rajahalme <jarno@covalent.io>

--- provision/envoy.sh
+++ provision/envoy.sh
@@ -13,8 +13,19 @@ sudo -u vagrant -E sh -c "\
     cd \"${GOPATH}/src/github.com/cilium\" && \
     git clone -b ${CILIUM_BRANCH} https://github.com/cilium/cilium.git && \
     cd cilium && \
-    git submodule update --init --recursive && \
-    cd envoy && \
+    git submodule update --init --recursive"
+
+export BAZEL_VERSION=`cat "${GOPATH}/src/github.com/cilium/cilium/envoy/BAZEL_VERSION"`
+
+# Install bazel
+wget -nv "https://github.com/bazelbuild/bazel/releases/download/${BAZEL_VERSION}/bazel-${BAZEL_VERSION}-installer-linux-x86_64.sh"
+chmod +x "bazel-${BAZEL_VERSION}-installer-linux-x86_64.sh"
+sudo -E "./bazel-${BAZEL_VERSION}-installer-linux-x86_64.sh"
+sudo -E mv /usr/local/bin/bazel /usr/bin
+rm "bazel-${BAZEL_VERSION}-installer-linux-x86_64.sh"
+
+sudo -u vagrant -E sh -c "\
+    cd \"${GOPATH}/src/github.com/cilium/cilium/envoy\" && \
     grep \"ENVOY_SHA[ \t]*=\" WORKSPACE | cut -d \\\" -f 2 >SOURCE_VERSION && \
     cat SOURCE_VERSION && \
     make && \
