729810acead1ac3f5a6b5aa01e303a8418891f8e
André Martins <andre@cilium.io>
2018-10-12 12:08:34 +0200

    provision: update image tags
    Signed-off-by: André Martins <andre@cilium.io>

--- provision/pull-images.sh
+++ provision/pull-images.sh
@@ -15,14 +15,15 @@ if [ -z "${NAME_PREFIX}" ]; then
         docker.io/cilium/cc-grpc-demo:v2.0 \
         docker.io/cilium/cilium:v1.0.6 \
         docker.io/cilium/cilium:v1.1.5 \
-        docker.io/cilium/cilium:v1.2.3 \
+        docker.io/cilium/cilium:v1.2.4 \
         docker.io/cilium/connectivity-container:v1.0 \
         docker.io/cilium/demo-client:latest \
         docker.io/cilium/demo-httpd:latest \
         docker.io/cilium/kafkaclient2:latest \
         docker.io/cilium/kafkaclient:latest \
-        docker.io/cilium/microscope:1.1.1-ci \
+        docker.io/cilium/microscope:1.1.2-ci \
         docker.io/cilium/starwars:v1.0 \
+        docker.io/coredns/coredns:1.2.2 \
         docker.io/coredns/coredns:1.0.6 \
         docker.io/digitalwonderland/zookeeper:latest \
         docker.io/istio/examples-bookinfo-details-v1:1.6.0 \
@@ -41,9 +42,10 @@ if [ -z "${NAME_PREFIX}" ]; then
         gcr.io/google_containers/k8s-dns-kube-dns-amd64:1.14.10 \
         gcr.io/google_containers/k8s-dns-sidecar-amd64:1.14.10 \
         gcr.io/google_samples/gb-redisslave:v1 \
-        quay.io/cilium/cilium-builder:2018-09-11 \
+        quay.io/cilium/cilium-builder:2018-09-17 \
         quay.io/cilium/cilium-runtime:2018-09-13 \
-        quay.io/coreos/etcd:v3.2.17; \
+        quay.io/coreos/etcd:v3.2.17 \
+        quay.io/coreos/etcd:v3.3.9; \
         do
           echo "pulling image: $img"
           sudo docker pull "${img}" &
