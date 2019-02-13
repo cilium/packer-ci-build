76001514e2bcac304c7d0c6ca45b47a912091df6
Michal Rostecki <mrostecki@opensuse.org>
2019-02-13 13:50:48 +0100

    opensuse: Switch to Tumbleweed release
    The devel:kubic repository has no intention of being usable on stable
    releases of openSUSE and packages needed for Cilium development are too
    fresh to be included in stable releases. The rolling-release version
    (Tumbleweed) needs to be used.
    
    Signed-off-by: Michal Rostecki <mrostecki@opensuse.org>

--- provision/opensuse/install.sh
+++ provision/opensuse/install.sh
@@ -4,9 +4,7 @@ set -eux
 
 source "${ENV_FILEPATH}"
 
-zypper ar -r https://download.opensuse.org/repositories/devel:/CaaSP:/Head:/ControllerNode/openSUSE_Leap_15.0/devel:CaaSP:Head:ControllerNode.repo
-zypper ar -r https://download.opensuse.org/repositories/devel:/kubic/openSUSE_Leap_15.0/devel:kubic.repo
-zypper ar -r https://download.opensuse.org/repositories/devel:/languages:/go/openSUSE_Leap_15.0/devel:languages:go.repo
+zypper ar -r https://download.opensuse.org/repositories/devel:/kubic/openSUSE_Tumbleweed/devel:kubic.repo
 zypper -n --gpg-auto-import-key in --no-recommends \
         autoconf \
         automake \
