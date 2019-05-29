45f15f146b5b6971cb5a0260fe280e0ee17a42b0
Michal Rostecki <mrostecki@opensuse.org>
2019-05-29 21:06:27 +0200

    Add sonobuoy
    sonobuoy is a diagnostic tool for checking the state of Kubernetes
    cluster with conformance tests. Cilium is going to have Jenkins jobs
    which run that tool.
    
    Ref: cilium/cilium#8161
    
    Signed-off-by: Michal Rostecki <mrostecki@opensuse.org>

--- provision/golang.sh
+++ provision/golang.sh
@@ -8,6 +8,7 @@ sudo -u vagrant -E bash -c "mkdir ${GOPATH} && \
 go get -u github.com/cilium/go-bindata/... && \
 go get -u github.com/google/gops && \
 go get github.com/subfuzion/envtpl/... && \
-go get -u github.com/gordonklaus/ineffassign"
+go get -u github.com/gordonklaus/ineffassign && \
+go get -u github.com/heptio/sonobuoy"
 
 sudo -E ln -s "${GOPATH}/bin/"* /usr/bin
