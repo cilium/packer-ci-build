e9b50fb7358f481a42a6fa565d9fbf7138c0ef93
Maciej Kwiek <maciej@isovalent.com>
2019-08-07 13:29:09 +0200

    Bumb cri-o version
    Signed-off-by: Maciej Kwiek <maciej@isovalent.com>

--- provision/ubuntu/crio.sh
+++ provision/ubuntu/crio.sh
@@ -4,11 +4,7 @@ source "${ENV_FILEPATH}"
 
 set -e
 
-sudo apt-key adv --recv-key --keyserver keyserver.ubuntu.com 8BECF1637AD8C79D
-
-cat <<EOF > /etc/apt/sources.list.d/projectatomic-ubuntu-ppa-xenial.list
-deb http://ppa.launchpad.net/projectatomic/ppa/ubuntu xenial main
-EOF
+sudo add-apt-repository ppa:projectatomic/ppa
 
 sudo apt-get update
-sudo apt-get install -y cri-o-1.10
+sudo apt-get install -y cri-o-1.12
