264f23fda9b4a0b292cd56c874d5da3f346132fd
Martynas Pumputis <m@lambda.lt>
2020-01-13 12:11:31 +0100

    ubuntu-next: Switch back to the bpf-next.git tree
    The patch [1] has already made to the tree.
    
    [1]: https://git.kernel.org/pub/scm/linux/kernel/git/davem/net.git/commit/?id=cc52d9140aa920d8d61c7f6de3fff5fea6692ea9
    
    Signed-off-by: Martynas Pumputis <m@lambda.lt>

--- provision/ubuntu/kernel-next-bpftool.sh
+++ provision/ubuntu/kernel-next-bpftool.sh
@@ -8,7 +8,7 @@ sudo apt-get install -y --allow-downgrades \
     pkg-config bison flex build-essential gcc libssl-dev \
     libelf-dev bc
 
-git clone --depth 1 git://git.kernel.org/pub/scm/linux/kernel/git/bpf/bpf.git $HOME/k
+git clone --depth 1 git://git.kernel.org/pub/scm/linux/kernel/git/bpf/bpf-next.git $HOME/k
 
 cd $HOME/k/tools/bpf/bpftool
 make
