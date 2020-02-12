fd3ea0d405b882d6ac0e17ea4dbe8a71b56d70ca
Michal Rostecki <mrostecki@opensuse.org>
2020-02-12 17:12:17 +0100

    Use Cilium fork of the kernel to build bpftool
    Cilium fork of the Linux kernel contains necessary enhancements for
    bpftool which are not avalavle upstream yet.
    
    Ref: cilium/cilium#10048
    
    Signed-off-by: Michal Rostecki <mrostecki@opensuse.org>

--- provision/ubuntu/kernel-next-bpftool.sh
+++ provision/ubuntu/kernel-next-bpftool.sh
@@ -9,7 +9,8 @@ sudo apt-get install -y --allow-downgrades \
     libelf-dev bc
 
 git clone --branch v5.4 --depth 1 git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git $HOME/k
+git clone --depth 1 -b bpftool https://github.com/cilium/linux.git $HOME/k-bpftool
 
-cd $HOME/k/tools/bpf/bpftool
+cd $HOME/k-bpftool/tools/bpf/bpftool
 make
 sudo make install
