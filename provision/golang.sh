58a27c978436bcb3da3fa90364ba4e5b8950a5de
Daniel Borkmann <daniel@iogearbox.net>
2020-02-28 15:16:10 +0100

    go get: remove github.com/cilium/go-bindata dependency
    The repo does not exist anymore.
    
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- provision/golang.sh
+++ provision/golang.sh
@@ -5,7 +5,6 @@ source "${ENV_FILEPATH}"
 set -e
 
 sudo -u vagrant -E bash -c "mkdir ${GOPATH} && \
-go get -u github.com/cilium/go-bindata/... && \
 go get -u github.com/google/gops && \
 go get github.com/subfuzion/envtpl/... && \
 go get -u github.com/gordonklaus/ineffassign"
