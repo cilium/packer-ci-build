ccd096fff8e326aba3600efefb7ae10319e7bc9e
Tobias Klauser <tklauser@distanz.ch>
2020-04-01 20:26:45 +0200

    provision: add prealloc static analysis tool
    Needed for cilium/cilium#10806
    
    Signed-off-by: Tobias Klauser <tklauser@distanz.ch>

--- provision/golang.sh
+++ provision/golang.sh
@@ -7,6 +7,7 @@ set -e
 sudo -u vagrant -E bash -c "mkdir ${GOPATH} && \
 go get -u github.com/google/gops && \
 go get github.com/subfuzion/envtpl/... && \
-go get -u github.com/gordonklaus/ineffassign"
+go get -u github.com/gordonklaus/ineffassign && \
+go get -u github.com/alexkohler/prealloc"
 
 sudo -E ln -s "${GOPATH}/bin/"* /usr/bin
