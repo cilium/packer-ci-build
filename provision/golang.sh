de231466fbb1b2b73e68ea3d66674d6d3a5f04ad
Michal Rostecki <mrostecki@opensuse.org>
2020-05-19 18:44:01 +0200

    golang: Add opennota/check tools
    Cilium is going to use the varcheck tool from opennota/check repository.
    
    Signed-off-by: Michal Rostecki <mrostecki@opensuse.org>

--- provision/golang.sh
+++ provision/golang.sh
@@ -7,6 +7,7 @@ set -e
 sudo -u vagrant -E bash -c "mkdir ${GOPATH} && \
 go get -u github.com/google/gops && \
 go get github.com/subfuzion/envtpl/... && \
-go get -u github.com/gordonklaus/ineffassign"
+go get -u github.com/gordonklaus/ineffassign && \
+go get -u gitlab.com/opennota/check/cmd/..."
 
 sudo -E ln -s "${GOPATH}/bin/"* /usr/bin
