cilium/packer-ci-build

commit 1717cf16b06e9c895dab8cd73531c2b74755aa2a

llvm: re-enable x86 next to bpf backend
Given this is used for dev VMs, it's useful to have x86 around as well
since it's used e.g. in the BPF unit tests that the CI runs. So running
it locally, x86 backend would be needed, too.

Note that this does not affect the cilium-runtime image which does not
have the x86 backend (and will never have it again).

Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

provision/ubuntu/install.sh
@@ -93,7 +93,7 @@ mkdir -p llvm/llvm/build/install
 cd llvm
 git checkout -b d941df363d1cb621a3836b909c37d79f2a3e27e2 d941df363d1cb621a3836b909c37d79f2a3e27e2
 cd llvm/build
-cmake .. -G "Ninja" -DLLVM_TARGETS_TO_BUILD="BPF" -DLLVM_ENABLE_PROJECTS="clang" -DBUILD_SHARED_LIBS=OFF -DCMAKE_BUILD_TYPE=Release -DLLVM_BUILD_RUNTIME=OFF
+cmake .. -G "Ninja" -DLLVM_TARGETS_TO_BUILD="BPF;X86" -DLLVM_ENABLE_PROJECTS="clang" -DBUILD_SHARED_LIBS=OFF -DCMAKE_BUILD_TYPE=Release -DLLVM_BUILD_RUNTIME=OFF
 ninja clang llc
 strip bin/clang
 strip bin/llc

commit 96abf4bcb38124549e1ec1cbf282d49d4088955a

provision: Install headers required for Clang
We need these header files to compile the BPF unit tests inside the
development VMs.

Related: https://github.com/cilium/cilium/issues/11255
Fixes: 1717cf1 ("llvm: re-enable x86 next to bpf backend")
Signed-off-by: Paul Chaignon <paul@cilium.io>

provision/ubuntu/install.sh
@@ -110,6 +110,7 @@ strip bin/clang
 strip bin/llc
 cp bin/clang /usr/bin/clang
 cp bin/llc /usr/bin/llc
+cp -n lib/clang/11.0.0/include/*.h /usr/include/
 cd ../../..
 rm -fr llvm/
 