43ada3948b2d962a446037f04040914454fd6b64
Paul Chaignon <paul@cilium.io>
2020-07-23 12:42:52 +0200

    provision: Update list of pulled Docker images
    Signed-off-by: Paul Chaignon <paul@cilium.io>

--- provision/pull-images.sh
+++ provision/pull-images.sh
@@ -19,32 +19,31 @@ if [ -z "${NAME_PREFIX}" ]; then
         k8s.gcr.io/coredns:1.3.1 \
         k8s.gcr.io/coredns:1.6.2 \
         k8s.gcr.io/coredns:1.6.5 \
-        docker.io/cilium/cc-grpc-demo:v2.0 \
+        docker.io/cilium/cc-grpc-demo:v3.0 \
         docker.io/cilium/demo-client:latest \
         docker.io/cilium/demo-httpd:latest \
         docker.io/cilium/echoserver:1.10.1 \
         docker.io/cilium/echoserver-udp:v2020.01.30 \
-        docker.io/cilium/istio_pilot:1.5.4 \
-        docker.io/cilium/istio_proxy:1.5.4 \
-        docker.io/cilium/json-mock:1.1 \
+        docker.io/cilium/istio_pilot:1.5.7 \
+        docker.io/cilium/istio_proxy:1.5.7 \
+        docker.io/cilium/json-mock:1.2 \
         docker.io/cilium/kafkaclient2:latest \
         docker.io/cilium/kafkaclient:latest \
         docker.io/cilium/log-gatherer:v1.0 \
         docker.io/cilium/migrate-svc-test:v0.0.1 \
         docker.io/cilium/starwars:v1.0 \
-        docker.io/cilium/python-bmemcached:v0.0.1 \
-        docker.io/cilium/dnssec-client:v0.1 \
+        docker.io/cilium/python-bmemcached:v0.0.2 \
+        docker.io/cilium/dnssec-client:v0.2 \
         docker.io/cilium/docker-bind:v0.3 \
         docker.io/digitalwonderland/zookeeper:latest \
-        docker.io/istio/examples-bookinfo-details-v1:1.6.0 \
+        docker.io/istio/examples-bookinfo-details-v1:1.16.2 \
         docker.io/istio/examples-bookinfo-productpage-v1:0.2.3 \
-        docker.io/istio/examples-bookinfo-ratings-v1:1.6.0 \
+        docker.io/istio/examples-bookinfo-ratings-v1:1.16.2 \
         docker.io/istio/examples-bookinfo-reviews-v1:1.6.0 \
         docker.io/istio/examples-bookinfo-reviews-v2:1.6.0 \
         docker.io/library/alpine:3.9 \
         docker.io/library/cassandra:3.11.3 \
-        docker.io/library/httpd:2.4.34 \
-        docker.io/library/memcached:1.5.11 \
+        docker.io/library/memcached:1.6.6-alpine \
         docker.io/library/redis:4.0.11 \
         docker.io/library/registry:2.6.2 \
         docker.io/spotify/kafkaproxy:latest \
@@ -52,11 +51,11 @@ if [ -z "${NAME_PREFIX}" ]; then
         docker.io/wurstmeister/kafka:2.11-0.11.0.3 \
         docker.io/metallb/controller:v0.8.2 \
         docker.io/metallb/speaker:v0.8.2 \
-        gcr.io/google-samples/gb-frontend:v4 \
+        gcr.io/google-samples/gb-frontend:v6 \
         gcr.io/google_samples/gb-redisslave:v1 \
-        quay.io/cilium/cilium-envoy:a8f292139e923b205525feb2c8a4377005904776 \
-        quay.io/cilium/cilium-builder:2020-06-08 \
-        quay.io/cilium/cilium-runtime:2020-06-08 \
+        quay.io/cilium/cilium-envoy:ccad480c59aa8b946d98aaf79f8e9e38d6731fdc \
+        quay.io/cilium/cilium-builder:2020-07-20 \
+        quay.io/cilium/cilium-runtime:2020-07-20 \
         quay.io/coreos/etcd:v3.2.17 \
         quay.io/coreos/etcd:v3.4.7 \
 
