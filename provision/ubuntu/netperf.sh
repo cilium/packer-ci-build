8d84cb36d7e3cdb1ed41bd67373c5c276912494b
Daniel Borkmann <daniel@iogearbox.net>
2020-08-31 16:01:26 +0200

    provision: add netperf and super_netperf to VMs
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

---
+++ provision/ubuntu/netperf.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -xe
+
+sudo apt-get install -y --allow-downgrades \
+    automake build-essential gcc
+
+git clone --depth 1 https://github.com/HewlettPackard/netperf.git $HOME/n
+cd $HOME/n/
+./autogen.sh
+./configure --prefix=/usr
+ln -s /bin/true /usr/bin/makeinfo
+make
+make install
+cd -
+rm -rf $HOME/n/
+
+git clone --depth 1 https://github.com/cilium/netperf.git $HOME/n
+chmod a+x $HOME/n/super_netperf
+cp $HOME/n/super_netperf /usr/bin/
+rm -rf $HOME/n/
