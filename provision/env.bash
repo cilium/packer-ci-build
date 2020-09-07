d8cd6581bf785e472b78871f18ee835c11092479
Tobias Klauser <tklauser@distanz.ch>
2020-09-07 16:39:45 +0200

    provision: update Go to 1.15.1
    Signed-off-by: Tobias Klauser <tklauser@distanz.ch>

--- provision/env.bash
+++ provision/env.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export GOLANG_VERSION="1.14.3"
+export GOLANG_VERSION="1.15.1"
 export ETCD_VERSION="v3.1.0"
 export DOCKER_COMPOSE_VERSION="1.16.1"
 export CONTAINERD_VERSION="1.2.4"
