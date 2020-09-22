23cb3657517c573fe7a0c9cb2d13d2804cf4d9f5
Tobias Klauser <tklauser@distanz.ch>
2020-09-22 13:34:31 +0200

    provision: update golangci-lint to 1.13.0
    Follow cilium/cilium#13245
    
    Suggested-by: Tam Mach <sayboras@yahoo.com>
    Signed-off-by: Tobias Klauser <tklauser@distanz.ch>

--- provision/golang.sh
+++ provision/golang.sh
@@ -8,6 +8,6 @@ sudo -u vagrant -E bash -c "mkdir ${GOPATH} && \
 go get -u github.com/google/gops && \
 go get github.com/subfuzion/envtpl/... "
 
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sudo sh -s -- -b ${GOPATH}/bin/ v1.27.0
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sudo sh -s -- -b ${GOPATH}/bin/ v1.31.0
 
 sudo -E ln -s "${GOPATH}/bin/"* /usr/bin
