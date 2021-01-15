26cb269ed619f3eef8974d14de44546075e87d66
Tobias Klauser <tklauser@distanz.ch>
2021-01-15 17:16:47 +0100

    provision/ubuntu: make sure to call git log without pager
    It seems that recent PR builds of the net-next image timed out due to
    `git log -n1` being started with pager enabled and then hanging
    indefinitely. Thus. explicitly disable the pager with `--no-pager`.
    
    Signed-off-by: Tobias Klauser <tklauser@distanz.ch>

--- provision/ubuntu/kernel-next-bpftool.sh
+++ provision/ubuntu/kernel-next-bpftool.sh
@@ -10,7 +10,7 @@ sudo apt-get install -y --allow-downgrades \
 
 git clone --depth 1 git://git.kernel.org/pub/scm/linux/kernel/git/bpf/bpf-next.git $HOME/k
 cd $HOME/k
-git log -n1
+git --no-pager log -n1
 
 cd $HOME/k/tools/bpf/bpftool
 make
