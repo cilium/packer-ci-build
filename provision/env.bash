a6c337ce8b7c3b1e642d15cb8fa816fe1557eb6f
Sebastian Wicki <sebastian@isovalent.com>
2021-02-10 11:53:49 +0100

    provision: Bump Hubble to latest master
    This pulls in the lastest master version of the Hubble CLI. This is
    needed to test the API changes to embedded Hubble in Cilium master,
    specifcially cilium/cilium#14785
    
    The pulled docker image remains at the released 0.7.1 version, as this
    is what the Cilium master Dockerfile pulls in.
    
    Signed-off-by: Sebastian Wicki <sebastian@isovalent.com>

--- provision/env.bash
+++ provision/env.bash
@@ -3,7 +3,7 @@
 export GOLANG_VERSION="1.15.7"
 export ETCD_VERSION="v3.1.0"
 export CONTAINERD_VERSION="1.3.4"
-export HUBBLE_VERSION="0.7.1"
+export HUBBLE_VERSION="181982c0"
 export SONOBUOY_VERSION="0.14.2"
 export PROTOC_VERSION="3.12.4"
 export HOME_DIR=/home/vagrant
