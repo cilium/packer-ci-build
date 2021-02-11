18ab30cfa83b98152094362a53d6f582c29ebb28
Sebastian Wicki <sebastian@isovalent.com>
2021-02-11 10:49:57 +0100

    provision: Bump Go version
    Signed-off-by: Sebastian Wicki <sebastian@isovalent.com>

--- provision/env.bash
+++ provision/env.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export GOLANG_VERSION="1.15.7"
+export GOLANG_VERSION="1.15.8"
 export ETCD_VERSION="v3.1.0"
 export CONTAINERD_VERSION="1.3.4"
 export HUBBLE_VERSION="181982c0"
