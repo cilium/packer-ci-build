ae3efca46e9a6ab3fa2ee7649f8601bb95fb1e44
Martynas Pumputis <m@lambda.lt>
2021-03-29 09:22:00 +0200

    Update cached docker images list
    - Update log-gatherer:v1.1 (new vsn includes tcpdump)
    - Add kube-wireguard:0.0.2.
    
    Signed-off-by: Martynas Pumputis <m@lambda.lt>

--- provision/pull-images.sh
+++ provision/pull-images.sh
@@ -29,7 +29,7 @@ if [ -z "${NAME_PREFIX}" ]; then
         docker.io/cilium/json-mock:1.2 \
         docker.io/cilium/kafkaclient2:1.0 \
         docker.io/cilium/kafkaclient:1.0 \
-        docker.io/cilium/log-gatherer:v1.0 \
+        docker.io/cilium/log-gatherer:v1.1 \
         docker.io/cilium/migrate-svc-test:v0.0.1 \
         docker.io/cilium/netperf:0.0.2 \
         docker.io/cilium/starwars:v1.0 \
@@ -53,6 +53,7 @@ if [ -z "${NAME_PREFIX}" ]; then
         gcr.io/google-samples/gb-frontend:v6 \
         gcr.io/google_samples/gb-redis-follower:v2 \
         quay.io/cilium/cilium-envoy:63de0bd958d05d82e2396125dcf6286d92464c56 \
+        quay.io/cilium/kube-wireguarder:0.0.2 \
         quay.io/cilium/net-test:v1.0.0 \
         quay.io/coreos/etcd:v3.4.7 \
 
