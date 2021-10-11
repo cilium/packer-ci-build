090595ea4c72b250542ab6b82c1540a0298aecfc
Tobias Klauser <tobias@cilium.io>
2021-10-11 11:47:45 +0200

    bpf-next-patches: revert bpf-next commit 354e8f1970f8 ("bpf: Support <8-byte scalar spill and refill")
    Reason for revert: currently breaks loading of Cilium's bpf_overlay.o
    using bpf-next kernel, see
    https://jenkins.cilium.io/job/Cilium-PR-Runtime-net-next/304
    
    Signed-off-by: Tobias Klauser <tobias@cilium.io>

--- provision/ubuntu/kernel-next.sh
+++ provision/ubuntu/kernel-next.sh
@@ -28,7 +28,7 @@ rm -rf pahole
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib/
 
 # Apply local patches
-git apply < /tmp/*.patch
+git apply /tmp/*.patch
 
 # Build kernel
 cp /boot/config-`uname -r` .config
