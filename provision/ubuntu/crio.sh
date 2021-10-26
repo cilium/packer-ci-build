0cd43a4af715388305291e3ec971350ad7d310fd
Paul Chaignon <paul@cilium.io>
2021-10-26 14:46:26 +0200

    provision: Update instructions to install CRI-O
    The previous instructions don't work with Ubuntu 20.04 anymore. The new
    instructions were copied from cri-o.io.
    
    Signed-off-by: Paul Chaignon <paul@cilium.io>

--- provision/ubuntu/crio.sh
+++ provision/ubuntu/crio.sh
@@ -4,7 +4,13 @@ source "${ENV_FILEPATH}"
 
 set -e
 
-sudo add-apt-repository ppa:projectatomic/ppa
+OS=xUbuntu_20.04
+VERSION=1.21
+echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/ /" > /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
+echo "deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/$VERSION/$OS/ /" > /etc/apt/sources.list.d/devel:kubic:libcontainers:stable:cri-o:$VERSION.list
 
-sudo apt-get update
-sudo apt-get install -y cri-o-1.12
+curl -L https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable:cri-o:$VERSION/$OS/Release.key | apt-key add -
+curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/Release.key | apt-key add -
+
+apt-get update
+apt-get install -y cri-o cri-o-runc
