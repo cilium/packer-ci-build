cilium/packer-ci-build

commit 29f1121d8778cadbb4847b29fdabb6404191b592

provision: Commit bpf-next patches
Commit the bpf-next patches instead of just applying changes, to avoid
the -dirty suffix on the kernel image's version string.

Signed-off-by: Paul Chaignon <paul@cilium.io>

provision/ubuntu/kernel-next.sh
@@ -28,7 +28,7 @@ rm -rf pahole
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib/
 
 # Apply local patches
-git apply /tmp/*.patch
+git am /tmp/*.patch
 
 # Build kernel
 cp /boot/config-`uname -r` .config

commit 62b7e0a5e15895c7e63e36f02769fdb87dbc1b17

provision: Fix git am for kernel patches
The 'git am' command currently fails with:

    *** Please tell me who you are.
    Run

      git config --global user.email "you@example.com"
      git config --global user.name "Your Name"

    to set your account's default identity.
    Omit --global to set the identity only in this repository.

This commit fixes it.

It's not clear why the build didn't fail in the initial pull request.

Fixes: 29f1121d ("provision: Commit bpf-next patches")
Signed-off-by: Paul Chaignon <paul@cilium.io>

provision/ubuntu/kernel-next.sh
@@ -28,6 +28,8 @@ rm -rf pahole
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib/
 
 # Apply local patches
+git config --global user.email "maintainer@cilium.io"
+git config --global user.name  "Cilium Maintainers"
 git am /tmp/*.patch
 
 # Build kernel