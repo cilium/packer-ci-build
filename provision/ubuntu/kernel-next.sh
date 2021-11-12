cilium/packer-ci-build

commit d1841cb1162ae91efbae11d5b9709ed880fdcc9c

provision: Specialize kernel config to current modules
The kernel build is filling the 40GB disk because we are compiling a ton
of drivers and modules we don't need. Using the localmodconfig target
fixes that and reduces the whole build to <10GB.

CONFIG_NETFILTER_ADVANCED, CONFIG_NF_CONNTRACK, and CONFIG_IP_VS are
required for CONFIG_NETFILTER_XT_MATCH_{CONNTRACK,IPVS} (itself required
for Docker). CONFIG_DM_THIN_PROVISIONING is required or Docker fails to
start with:

    Error running DeviceCreate (createPool) dm_task_run failed

Signed-off-by: Paul Chaignon <paul@cilium.io>

provision/ubuntu/kernel-next.sh
@@ -32,7 +32,7 @@ git am /tmp/*.patch
 
 # Build kernel
 cp /boot/config-`uname -r` .config
-make olddefconfig && make prepare
+yes "" | make localyesconfig && make prepare
 
 ./scripts/config --enable CONFIG_LOCALVERSION_AUTO
 ./scripts/config --disable CONFIG_WERROR
@@ -63,6 +63,24 @@ make olddefconfig && make prepare
 ./scripts/config --enable CONFIG_DEBUG_INFO_BTF_MODULES
 ./scripts/config --disable CONFIG_SYSTEM_TRUSTED_KEYS
 ./scripts/config --disable CONFIG_SYSTEM_REVOCATION_KEYS
+# Needed by VirtualBox to load the Guest Additions.
+./scripts/config --enable CONFIG_ISO9660_FS
+# Needed for Docker.
+./scripts/config --enable CONFIG_VETH
+./scripts/config --enable CONFIG_BRIDGE
+./scripts/config --module CONFIG_BRIDGE_NETFILTER
+./scripts/config --module CONFIG_IP_NF_FILTER
+./scripts/config --module CONFIG_IP_NF_TARGET_MASQUERADE
+./scripts/config --module CONFIG_NETFILTER_XT_MATCH_ADDRTYPE
+./scripts/config --module CONFIG_NETFILTER_XT_MATCH_CONNTRACK
+./scripts/config --module CONFIG_NETFILTER_XT_MATCH_IPVS
+./scripts/config --module CONFIG_NETFILTER_ADVANCED
+./scripts/config --enable CONFIG_NF_CONNTRACK
+./scripts/config --enable CONFIG_IP_VS
+./scripts/config --module CONFIG_NETFILTER_XT_MARK
+./scripts/config --module CONFIG_IP_NF_NAT
+./scripts/config --module CONFIG_NF_NAT
+./scripts/config --enable CONFIG_DM_THIN_PROVISIONING
 
 make -j$(nproc) deb-pkg
 cd ..

commit 994604e0d328785586a28e40edac33f24d25255f

provision: Fix CONFIG_NETFILTER_ADVANCED value
Kernel option CONFIG_NETFILTER_ADVANCED takes a boolean value and
doesn't accept 'module'. This error causes other options to not be
enabled.

Fixes: d1841cb1 ("provision: Specialize kernel config to current modules")
Signed-off-by: Paul Chaignon <paul@cilium.io>

provision/ubuntu/kernel-next.sh
@@ -76,7 +76,7 @@ yes "" | make localyesconfig && make prepare
 ./scripts/config --module CONFIG_NETFILTER_XT_MATCH_ADDRTYPE
 ./scripts/config --module CONFIG_NETFILTER_XT_MATCH_CONNTRACK
 ./scripts/config --module CONFIG_NETFILTER_XT_MATCH_IPVS
-./scripts/config --module CONFIG_NETFILTER_ADVANCED
+./scripts/config --enable CONFIG_NETFILTER_ADVANCED
 ./scripts/config --enable CONFIG_NF_CONNTRACK
 ./scripts/config --enable CONFIG_IP_VS
 ./scripts/config --module CONFIG_NETFILTER_XT_MARK