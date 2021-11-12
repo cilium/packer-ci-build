cilium/packer-ci-build

commit d1841cb1162ae91efbae11d5b9709ed880fdcc9c

provision: Specialize kernel config to current modules
The kernel build is filling the 40GB disk because we are compiling a ton
of drivers and modules we don't need. Using the localmodconfig target
fixes that and reduces the whole build to <10GB.

CONFIG_NETFILTER_ADVANCED, CONFIG_NF_CONNTRACK, and CONFIG_IP_VS are
required for CONFIG_NETFILTER_XT_MATCH_{CONNTRACK,IPVS} (itself required
for Docker). CONFIG_DM_THIN_PROVISIONING is required or Docker fails to
start with:

    Error running DeviceCreate (createPool) dm_task_run failed

Signed-off-by: Paul Chaignon <paul@cilium.io>

provision/ubuntu/kernel-next.sh
@@ -32,7 +32,7 @@ git am /tmp/*.patch
 
 # Build kernel
 cp /boot/config-`uname -r` .config
-make olddefconfig && make prepare
+yes "" | make localyesconfig && make prepare
 
 ./scripts/config --enable CONFIG_LOCALVERSION_AUTO
 ./scripts/config --disable CONFIG_WERROR
@@ -63,6 +63,24 @@ make olddefconfig && make prepare
 ./scripts/config --enable CONFIG_DEBUG_INFO_BTF_MODULES
 ./scripts/config --disable CONFIG_SYSTEM_TRUSTED_KEYS
 ./scripts/config --disable CONFIG_SYSTEM_REVOCATION_KEYS
+# Needed by VirtualBox to load the Guest Additions.
+./scripts/config --enable CONFIG_ISO9660_FS
+# Needed for Docker.
+./scripts/config --enable CONFIG_VETH
+./scripts/config --enable CONFIG_BRIDGE
+./scripts/config --module CONFIG_BRIDGE_NETFILTER
+./scripts/config --module CONFIG_IP_NF_FILTER
+./scripts/config --module CONFIG_IP_NF_TARGET_MASQUERADE
+./scripts/config --module CONFIG_NETFILTER_XT_MATCH_ADDRTYPE
+./scripts/config --module CONFIG_NETFILTER_XT_MATCH_CONNTRACK
+./scripts/config --module CONFIG_NETFILTER_XT_MATCH_IPVS
+./scripts/config --module CONFIG_NETFILTER_ADVANCED
+./scripts/config --enable CONFIG_NF_CONNTRACK
+./scripts/config --enable CONFIG_IP_VS
+./scripts/config --module CONFIG_NETFILTER_XT_MARK
+./scripts/config --module CONFIG_IP_NF_NAT
+./scripts/config --module CONFIG_NF_NAT
+./scripts/config --enable CONFIG_DM_THIN_PROVISIONING
 
 make -j$(nproc) deb-pkg
 cd ..

commit f38c2cf9133b5ea742e8924df272997fcf184080

provision: Add missing kernel configs to run Cilium
Commit d1841cb1 specialized the kernel config to reduce the size of the
compiled kernel on disk. Running the resulting VM image through Cilium's
CI however uncovered many missing kernel configs. For example, the
bandwidth manager and IPsec would fail due to missing kernel support;
tests using dummy devices would fail similarly. In addition, kernel
configs were missing to enable NFS (for VBox shared folders) and basic
networking inside the VMs (missing virtio-net support).

This new configuration was tested locally by replacing the usual Vagrant
VM image with the newly-built image and running the full set of K8s and
Runtime tests on this image. A couple tests (e.g., K8sIstio) still fail,
but they seem unrelated to kernel support, so maybe some other issue
with running locally.

Fixes: d1841cb1 ("provision: Specialize kernel config to current modules")
Signed-off-by: Paul Chaignon <paul@cilium.io>

provision/ubuntu/kernel-next.sh
@@ -65,6 +65,7 @@ yes "" | make localyesconfig && make prepare
 ./scripts/config --enable CONFIG_DEBUG_INFO_BTF_MODULES
 ./scripts/config --disable CONFIG_SYSTEM_TRUSTED_KEYS
 ./scripts/config --disable CONFIG_SYSTEM_REVOCATION_KEYS
+./scripts/config --enable CONFIG_VIRTIO_NET
 # Needed by VirtualBox to load the Guest Additions.
 ./scripts/config --enable CONFIG_ISO9660_FS
 # Needed for Docker.
@@ -83,6 +84,61 @@ yes "" | make localyesconfig && make prepare
 ./scripts/config --module CONFIG_IP_NF_NAT
 ./scripts/config --module CONFIG_NF_NAT
 ./scripts/config --enable CONFIG_DM_THIN_PROVISIONING
+# Needed for Kubernetes.
+./scripts/config --module CONFIG_IP_NF_TARGET_REDIRECT
+./scripts/config --module CONFIG_NETFILTER_XT_MATCH_COMMENT
+# Needed for Cilium.
+./scripts/config --enable CONFIG_IP_NF_MANGLE
+./scripts/config --enable CONFIG_IP_NF_RAW
+./scripts/config --enable CONFIG_IP6_NF_IPTABLES
+./scripts/config --enable CONFIG_IP6_NF_FILTER
+./scripts/config --enable CONFIG_IP6_NF_MANGLE
+./scripts/config --enable CONFIG_IP6_NF_RAW
+./scripts/config --enable CONFIG_IP6_NF_NAT
+./scripts/config --enable CONFIG_IP6_NF_TARGET_MASQUERADE
+./scripts/config --module CONFIG_NETFILTER_XT_TARGET_TPROXY
+./scripts/config --module CONFIG_NETFILTER_XT_MATCH_MARK
+./scripts/config --module CONFIG_NETFILTER_XT_MATCH_SOCKET
+./scripts/config --module CONFIG_NETFILTER_XT_TARGET_CT
+./scripts/config --module CONFIG_IP_SET
+./scripts/config --module CONFIG_IP_SET_HASH_IP
+./scripts/config --module CONFIG_NETFILTER_XT_SET
+./scripts/config --module CONFIG_VXLAN
+./scripts/config --module CONFIG_GENEVE
+./scripts/config --module CONFIG_NET_SCH_FQ
+# Needed for Cilium's IPsec implementation.
+./scripts/config --enable CONFIG_XFRM
+./scripts/config --enable CONFIG_XFRM_OFFLOAD
+./scripts/config --enable CONFIG_XFRM_STATISTICS
+./scripts/config --module CONFIG_XFRM_ALGO
+./scripts/config --module CONFIG_XFRM_USER
+./scripts/config --module CONFIG_INET_ESP
+./scripts/config --module CONFIG_INET_IPCOMP
+./scripts/config --module CONFIG_INET_XFRM_TUNNEL
+./scripts/config --module CONFIG_INET_TUNNEL
+./scripts/config --module CONFIG_INET6_ESP
+./scripts/config --module CONFIG_INET6_IPCOMP
+./scripts/config --module CONFIG_INET6_XFRM_TUNNEL
+./scripts/config --module CONFIG_INET6_TUNNEL
+./scripts/config --module CONFIG_INET_XFRM_MODE_TUNNEL
+./scripts/config --module CONFIG_CRYPTO_AEAD
+./scripts/config --module CONFIG_CRYPTO_AEAD2
+./scripts/config --module CONFIG_CRYPTO_GCM
+./scripts/config --module CONFIG_CRYPTO_SEQIV
+./scripts/config --module CONFIG_CRYPTO_CBC
+./scripts/config --module CONFIG_CRYPTO_HMAC
+./scripts/config --module CONFIG_CRYPTO_SHA256
+./scripts/config --module CONFIG_CRYPTO_AES
+# Needed for Cilium's tests.
+./scripts/config --module CONFIG_NF_CT_NETLINK
+./scripts/config --module CONFIG_DUMMY
+./scripts/config --module CONFIG_BONDING
+./scripts/config --module CONFIG_VLAN_8021Q
+# Needed for NFS shared folders.
+./scripts/config --module CONFIG_NFS_FS
+./scripts/config --module CONFIG_NFS_V3
+./scripts/config --module CONFIG_NFSD
+./scripts/config --enable CONFIG_NFSD_V3
 
 yes "" | make config
 make -j$(nproc) deb-pkg