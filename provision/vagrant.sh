a6e87536d213a371b70385c0620dbae72d14ed2c
Jarno Rajahalme <jarno@isovalent.com>
2021-12-08 04:51:54 -0800

    provision: Do not overwrite SSH authorized keys
    Append to SSH authorized keys instead of overwriting them to keep any
    authorized keys set previously.
    
    This allows building of local images that have local users public key
    as an authorized key for local testing.
    
    Signed-off-by: Jarno Rajahalme <jarno@isovalent.com>

--- provision/vagrant.sh
+++ provision/vagrant.sh
@@ -9,9 +9,11 @@ source "${ENV_FILEPATH}"
 pubkey_url="https://raw.githubusercontent.com/hashicorp/vagrant/master/keys/vagrant.pub";
 mkdir -p "${HOME_DIR}/.ssh";
 if command -v wget >/dev/null 2>&1; then
-    wget --no-check-certificate "${pubkey_url}" -O "${HOME_DIR}/.ssh/authorized_keys";
+    wget --no-check-certificate "${pubkey_url}" -O vagrant.pub;
+    cat vagrant.pub >> "${HOME_DIR}/.ssh/authorized_keys";
+    rm vagrant.pub;
 elif command -v curl >/dev/null 2>&1; then
-    curl --insecure --location "${pubkey_url}" > "${HOME_DIR}/.ssh/authorized_keys";
+    curl --insecure --location "${pubkey_url}" >> "${HOME_DIR}/.ssh/authorized_keys";
 else
     echo "Cannot download vagrant public key";
     exit 1;
