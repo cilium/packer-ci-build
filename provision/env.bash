3753f78d5e053dbbd9bfa6c7fe0aa303315dbf2d
Jarno Rajahalme <jarno@isovalent.com>
2021-12-08 05:12:25 -0800

    build: Bump versions for arm64
    Use versions that have arm64 images available.
    
    Signed-off-by: Jarno Rajahalme <jarno@isovalent.com>

--- provision/env.bash
+++ provision/env.bash
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 export GOLANG_VERSION="1.17.5"
-export ETCD_VERSION="v3.1.0"
-export CONTAINERD_VERSION="1.3.4"
+export ETCD_VERSION="v3.2.32"
+export CONTAINERD_VERSION="1.6.0-beta.3"
 export HUBBLE_VERSION="0.9.0"
-export SONOBUOY_VERSION="0.14.2"
+export SONOBUOY_VERSION="0.16.5"
 export PROTOC_VERSION="3.12.4"
 export HOME_DIR=/home/vagrant
 export HOME=/home/vagrant
