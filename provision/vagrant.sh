0a39c87b1c532edcceca0a82e0e918312ed83f57
Jarno Rajahalme <jarno@isovalent.com>
2021-12-08 04:43:55 -0800

    provision: Generalize user name
    Pass the install user name via $USERNAME, so that same scripts can be
    used for different default user names (e.g., 'ubuntu' in addition to
    'vagrant').
    
    Signed-off-by: Jarno Rajahalme <jarno@isovalent.com>

--- provision/vagrant.sh
+++ provision/vagrant.sh
@@ -18,7 +18,7 @@ else
 fi
 chmod -R go-rwsx "${HOME_DIR}/.ssh";
 
-echo 'vagrant ALL=(ALL) NOPASSWD:ALL' >/etc/sudoers.d/99_vagrant
+echo "${USERNAME} ALL=(ALL) NOPASSWD:ALL" >/etc/sudoers.d/99_vagrant
 sudo chmod 440 /etc/sudoers.d/99_vagrant
 
 sudo sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=sudo' /etc/sudoers
