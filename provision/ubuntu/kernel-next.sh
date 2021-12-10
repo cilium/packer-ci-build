31a2a8370269735d003ac3caf9b6332032788abd
Paul Chaignon <paul@cilium.io>
2021-12-10 22:45:35 +0100

    bpf-next: Fix incorrect dead code elimination
    The latest bpf-next sources include a bug introduced by 354e8f1970f8
    ("bpf: Support <8-byte scalar spill and refill") that causes incorrect
    dead code elimination on Cilium's datapath. That in turn causes CT state
    entries to be missing and reply packets to be dropped in CI tests.
    
    This regression is fixed in 345e004d0233 ("bpf: Fix incorrect state
    pruning for <8B spill/fill") includes in this commit. We need to include
    it as a separate patch until it lands in bpf-next (currently in bpf).
    
    Signed-off-by: Paul Chaignon <paul@cilium.io>

--- provision/ubuntu/kernel-next.sh
+++ provision/ubuntu/kernel-next.sh
@@ -27,6 +27,11 @@ rm -rf pahole
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib/
 
+# Apply local patches
+git config --global user.email "maintainer@cilium.io"
+git config --global user.name  "Cilium Maintainers"
+git am /tmp/*.patch
+
 # Build kernel
 cp /boot/config-`uname -r` .config
 yes "" | make localyesconfig && make prepare
