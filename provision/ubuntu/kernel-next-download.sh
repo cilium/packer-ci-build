2baaaaef2190e6dee6019dd9857ccdfefb4050de
Paul Chaignon <paul@cilium.io>
2022-04-06 18:39:34 +0200

    next: Install all packages first
    While building perf, we get a bunch of warnings such as the following
    because we are missing dependencies:
    
        ==> virtualbox-iso: Makefile.config:441: No libdw DWARF unwind found, Please install elfutils-devel/libdw-dev >= 0.158 and/or set LIBDW_DIR
        ==> virtualbox-iso: Makefile.config:446: No libdw.h found or old libdw.h found or elfutils is older than 0.138, disables dwarf support. Please install new elfutils-devel/libdw-dev
    
    We can easily fix this by installing all packages before we build perf.
    
    Signed-off-by: Paul Chaignon <paul@cilium.io>

--- provision/ubuntu/kernel-next-download.sh
+++ provision/ubuntu/kernel-next-download.sh
@@ -6,7 +6,7 @@ export 'KCONFIG'=${KCONFIG:-"config-`uname -r`"}
 
 sudo apt-get install -y --allow-downgrades \
     pkg-config bison flex build-essential gcc libssl-dev \
-    libelf-dev bc libbfd-dev
+    libelf-dev bc libbfd-dev cmake libdw-dev git
 
 git clone --depth 1 git://git.kernel.org/pub/scm/linux/kernel/git/bpf/bpf-next.git $HOME/k
 cd $HOME/k
