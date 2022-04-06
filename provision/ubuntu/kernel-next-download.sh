37cc3759d28ae49b93213655ef8b9fc38fef561d
Paul Chaignon <paul@cilium.io>
2022-04-06 19:24:46 +0200

    next: Fix perf build
    The perf build is broken in bpf-next (see [1]). We need to cherry-pick a
    commit from the perf tree to fix it.
    
    1 - https://lore.kernel.org/lkml/5B37F7F6-CDFD-49B9-9BF6-CEA726E5F0CF@gmail.com/T/
    Signed-off-by: Paul Chaignon <paul@cilium.io>

--- provision/ubuntu/kernel-next-download.sh
+++ provision/ubuntu/kernel-next-download.sh
@@ -11,3 +11,9 @@ sudo apt-get install -y --allow-downgrades \
 git clone --depth 1 git://git.kernel.org/pub/scm/linux/kernel/git/bpf/bpf-next.git $HOME/k
 cd $HOME/k
 git --no-pager log -n1
+# We need this commit to fix a regression in perf's build.
+git remote add tip https://git.kernel.org/pub/scm/linux/kernel/git/tip/tip.git
+git fetch tip
+git config --global user.email "maintainer@cilium.io"
+git config --global user.name "Cilium Maintainers"
+git cherry-pick d4ff92659244a4783e424fa41b6f83645d8920c1
