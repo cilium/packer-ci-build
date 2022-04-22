4a8589699126541dbbab7dfe958fb5052e31ffe7
Tobias Klauser <tobias@cilium.io>
2022-04-22 12:46:41 +0200

    provision: update Go to 1.18.1, golangci-lint to 1.45.2
    Matching the versions currently used on Cilium `master` branch.
    
    Signed-off-by: Tobias Klauser <tobias@cilium.io>

--- provision/golang.sh
+++ provision/golang.sh
@@ -8,6 +8,6 @@ sudo -u vagrant -E bash -c "mkdir -p ${GOPATH} && \
 go install github.com/google/gops@latest && \
 go install github.com/subfuzion/envtpl/cmd/envtpl@latest"
 
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sudo sh -s -- -b ${GOPATH}/bin/ v1.42.1
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sudo sh -s -- -b ${GOPATH}/bin/ v1.45.2
 
 sudo -E ln -s "${GOPATH}/bin/"* /usr/bin
