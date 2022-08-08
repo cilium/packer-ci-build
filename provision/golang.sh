1249280f8b946a4d3df1721602b5b6133a0fd065
Tobias Klauser <tobias@cilium.io>
2022-08-08 14:17:28 +0200

    Update Go to 1.19, golangci-lint to 1.48.0
    Signed-off-by: Tobias Klauser <tobias@cilium.io>

--- provision/golang.sh
+++ provision/golang.sh
@@ -9,6 +9,6 @@ go install github.com/google/gops@latest && \
 go install github.com/subfuzion/envtpl/cmd/envtpl@latest && \
 go install github.com/mfridman/tparse@latest"
 
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sudo sh -s -- -b ${GOPATH}/bin/ v1.45.2
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sudo sh -s -- -b ${GOPATH}/bin/ v1.48.0
 
 sudo -E ln -s "${GOPATH}/bin/"* /usr/bin
