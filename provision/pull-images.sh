b01eba36d1c468c606c63b045ff0be6b770ba896
Tobias Klauser <tobias@cilium.io>
2022-08-08 14:15:41 +0200

    provision: drop unused test images
    The tests using these images have been removed from the Cilium repo.
    
    Signed-off-by: Tobias Klauser <tobias@cilium.io>

--- provision/pull-images.sh
+++ provision/pull-images.sh
@@ -16,18 +16,14 @@ if [ -z "${NAME_PREFIX}" ]; then
         docker.io/cilium/demo-httpd:1.0 \
         docker.io/cilium/dnssec-client:v0.2 \
         docker.io/cilium/docker-bind:v0.3 \
-        docker.io/cilium/dummylb:0.0.1 \
         docker.io/cilium/echoserver:1.10.1 \
         docker.io/cilium/echoserver-udp:v2020.01.30 \
-        docker.io/cilium/graceful-termination-test-apps:1.0.0 \
         docker.io/cilium/json-mock:1.2 \
-        docker.io/cilium/kafkaclient2:1.0 \
         docker.io/cilium/kafkaclient:1.0 \
         docker.io/cilium/kafkaproxy:1.0 \
         docker.io/cilium/log-gatherer:v1.1 \
         docker.io/cilium/migrate-svc-test:v0.0.2 \
         docker.io/cilium/netperf:2.0 \
-        docker.io/cilium/starwars:v1.0 \
         docker.io/cilium/zookeeper:1.0 \
         docker.io/frrouting/frr:v7.5.1 \
         docker.io/istio/examples-bookinfo-details-v1:1.16.2 \
@@ -36,11 +32,9 @@ if [ -z "${NAME_PREFIX}" ]; then
         docker.io/istio/examples-bookinfo-reviews-v1:1.6.0 \
         docker.io/istio/examples-bookinfo-reviews-v2:1.6.0 \
         docker.io/library/busybox:1.31.1 \
-        docker.io/library/cassandra:3.11.3 \
         docker.io/library/golang:${GOLANG_VERSION} \
         docker.io/library/redis:6.0.5 \
         docker.io/networkstatic/iperf3@sha256:e9bbc8312edff13e2ecccad0907db4b35119139e133719138108955cf07f0683 \
-        docker.io/tgraf/netperf:v1.0 \
         docker.io/wurstmeister/kafka:2.11-0.11.0.3 \
         gcr.io/google-samples/gb-frontend:v6 \
         gcr.io/google_samples/gb-redis-follower:v2 \
