971ec2456f6c6a66b7d37ff486b769cc643c06f9
Quentin Monnet <quentin@isovalent.com>
2022-10-03 12:36:36 +0100

    provision: Update apt cache before trying to install packages
    We have observed failures to build the VM images because the script to
    provision netperf would try to install packages pulling linux-libc-dev
    as a dependency, but in a version which is missing from the
    repositories.
    
    Call "apt-get update" before trying to "apt-get install" packages, in
    case no other script has run it before.
    
    Signed-off-by: Quentin Monnet <quentin@isovalent.com>

--- provision/ubuntu/kernel-next-download.sh
+++ provision/ubuntu/kernel-next-download.sh
@@ -4,6 +4,7 @@ set -xe
 
 export 'KCONFIG'=${KCONFIG:-"config-`uname -r`"}
 
+sudo apt-get update
 sudo apt-get install -y --allow-downgrades \
     pkg-config bison flex build-essential gcc libssl-dev \
     libelf-dev bc libbfd-dev cmake libdw-dev git
