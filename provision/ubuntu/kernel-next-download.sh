dadab8482aa86bcfb0186777508f0ec739fb8773
Tobias Klauser <tobias@cilium.io>
2023-02-24 15:42:19 +0100

    provision/ubuntu: pin bpf-next to last known
    The current bpf-next kernel is failing to shut down the VM for some
    reason, see
    https://jenkins.cilium.io/view/Packer%20builds/job/Vagrant-Master-Boxes-Packer-Build-net-next/160/
    
    For now to work around this, pin to the commit used in the
    last successful VM image build:
    https://jenkins.cilium.io/view/Packer%20builds/job/Vagrant-PR-Boxes-Packer-Build-Next/378/
    
    Signed-off-by: Tobias Klauser <tobias@cilium.io>

--- provision/ubuntu/kernel-next-download.sh
+++ provision/ubuntu/kernel-next-download.sh
@@ -9,6 +9,10 @@ sudo apt-get install -y --allow-downgrades \
     pkg-config bison flex build-essential gcc libssl-dev \
     libelf-dev bc libbfd-dev cmake libdw-dev git
 
-git clone --depth 1 git://git.kernel.org/pub/scm/linux/kernel/git/bpf/bpf-next.git $HOME/k
+# FIXME: current bpf-next kernel is failing to shut down the VM. For now to work around this, pin to
+# the commit used in the last successful VM image build:
+# https://jenkins.cilium.io/view/Packer%20builds/job/Vagrant-PR-Boxes-Packer-Build-Next/378/
+git clone --shallow-since 2023-01-17 git://git.kernel.org/pub/scm/linux/kernel/git/bpf/bpf-next.git $HOME/k
 cd $HOME/k
+git checkout c0f264e4edb60db410462d513e29d2de3ef7de56
 git --no-pager log -n1
